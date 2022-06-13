a5ddbdb5c2c58201038453d682e018a3167fccbd
Arjay Jones <arjay.jones@nlucis.dev>
2022-06-12 19:13:28 -0700

    getting started state

---
+++ Script/code.js
@@ -0,0 +1,11 @@
+const WIDTH = window.outerWidth;
+const HEIGHT = window.outerHeight;
+
+function setup() {
+  createCanvas(888, 888);
+}
+
+function draw() {
+  background(220);
+  ellipse(50, 50, 24, 24);
+}
