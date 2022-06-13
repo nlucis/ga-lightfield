e399d5b6849fa78f6d0dcff632711256b33d1e38
Arjay Jones <arjay.jones@nlucis.dev>
2022-06-13 15:28:00 -0700

    added toggle for setting framerate to 1 or 60

--- Script/cheese.js
+++ Script/cheese.js
@@ -2,19 +2,22 @@
 const WIDTH = window.outerWidth;
 const HEIGHT = window.outerHeight;
 
+// Interface variables
 var myColor, myNumber, myChoice; // cannot be let or const or GUI wont show anything
+var animated;
 
 function setup() {
   createCanvas(888, 888);
-  frameRate(24);
+  frameRate(animated ? 60 : 1);
 
   const GUI = createGui(':: INTERFACE ::');
   // Globals exposed to/by GUI
   myNumber = 100;
   myColor = color(255, 0, 0);
   myChoice = ['one', 'two', 'three'];
+  animated = false;
   // Add em in...
-  GUI.addGlobals('myColor', 'myNumber', 'myChoice', 'WIDTH');
+  GUI.addGlobals('myColor', 'myNumber', 'myChoice', 'WIDTH', 'animated');
 }
 
 function draw() {
