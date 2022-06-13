d1f205aca8176140668ad9a6b59ee461b189960d
Arjay Jones <arjay.jones@nlucis.dev>
2022-06-13 14:50:22 -0700

    adding working example for setting up GUI controls

--- Script/cheese.js
+++ Script/cheese.js
@@ -1,11 +1,23 @@
+// Global constants - Ignored by GUI
 const WIDTH = window.outerWidth;
 const HEIGHT = window.outerHeight;
 
+var myColor, myNumber, myChoice; // cannot be let or const or GUI wont show anything
+
 function setup() {
   createCanvas(888, 888);
+  frameRate(24);
+
+  const GUI = createGui(':: INTERFACE ::');
+  // Globals exposed to/by GUI
+  myNumber = 100;
+  myColor = color(255, 0, 0);
+  myChoice = ['one', 'two', 'three'];
+  // Add em in...
+  GUI.addGlobals('myColor', 'myNumber', 'myChoice', 'WIDTH');
 }
 
 function draw() {
-  background(220);
+  background(255);
   ellipse(50, 50, 24, 24);
 }
